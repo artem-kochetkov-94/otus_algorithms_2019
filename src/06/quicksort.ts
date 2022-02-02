cbd7fe9d3b3760995d6c4fffde974bcb41fa81ac
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2022-02-02 12:01:12 +0300

    fixes

--- src/06/quicksort.ts
+++ src/06/quicksort.ts
@@ -7,16 +7,11 @@ export function quickSort(arr: number[]): number[] {
     }
 
     const n = Math.floor(arr.length / 2);
-
-    for (let i = 0; i < n; i++) {
-        if (arr[i] < arr[n]) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+    for (let i = 0; i < arr.length; i++) {
+        if (i === n) {
+            continue;
         }
-    }
 
-    for (let i = n + 1; i < arr.length; i++) {
         if (arr[i] < arr[n]) {
             left.push(arr[i]);
         } else {
@@ -27,4 +22,4 @@ export function quickSort(arr: number[]): number[] {
     return [...quickSort(left), arr[n], ...quickSort(right)];
 }
 
-quickSort([3, 6, 1, 2, 9, 11, 13, 4, 6, 23, 2]);
+console.log(quickSort([3, 6, 1, 2, 9, 11, 13, 4, 6, 23, 2]));
