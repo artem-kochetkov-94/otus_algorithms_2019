7fe5b0954d705743e21fc0648d1ade7d2a18c218
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2022-02-02 23:32:35 +0300

    update hashTable

--- src/12/hashTable.ts
+++ src/12/hashTable.ts
@@ -1,7 +1,12 @@
 const CRC32 = require("crc-32");
 
-class HashTable {
-    internal: Array<string[]>;
+type List = {
+    data: string[];
+    next?: List;
+}
+
+export class HashTable {
+    internal: Array<List>;
 
     constructor() {
         this.internal = [];
@@ -11,26 +16,51 @@ class HashTable {
         const hash = CRC32.str(key);
         const index = Math.abs(hash) % 1000;
 
-        console.log('hash', hash);
-        console.log('index', index);
+        if (!this.internal[index]) {
+            this.internal[index] = {
+                data: [key, value],
+            };
 
-        this.internal[index] = [key, value];
+            return;
+        }
+
+        // при повторном индексе добавляем элементы в список
+        this.insertInList(this.internal[index], key, value);
+        console.log('this.internal', this.internal);
+    }
+
+    insertInList(list: List, key: string, value: string) {
+        // в случае если ключи совпадают - переписываем значение
+        if (list.data[0] === key) {
+            list.data[1] = value;
+            return;
+        }
+
+        if (!list.next) {
+            list.next = {
+                data: [key, value],
+            }
+
+            return;
+        }
+
+        this.insertInList(list.next, key, value);
     }
 
     get(key: string) {
         const hash = CRC32.str(key);
         const index = Math.abs(hash) % 1000;
 
-        console.log('this.internal[index]', this.internal[index]);
+        return this.findItem(this.internal[index], key);
+    }
 
-        return this.internal[index];
+    findItem(list, key) {
+        if (list.data[0] === key) {
+            return list.data;
+        }
+
+        return this.findItem(list.next, key);
     }
 }
 
-
-
 const hashTable = new HashTable();
-
-hashTable.insert('key1', 'value1');
-hashTable.insert('key2', 'value2');
-hashTable.get('key2');
